eb5e909adaa69d395c13ee1284e93ccc6462e617
Nickolai <lindeman072@mail.ru>
2023-07-13 14:15:44 +0400

    fix index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="react-mesto-auth">
+    <BrowserRouter basename="/react-mesto-auth">
       <App />
     </BrowserRouter>
   </React.StrictMode>
